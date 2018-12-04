4764315fe042335e11d95013e54ab9a61e285955
Margaux Delayre <mcsqueeze@gmail.com>
2018-12-04 17:49:34 +0100

    navbar and signin form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :photo, PhotoUploader
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :meals, dependent: :destroy
