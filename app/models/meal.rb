66bcd32aabcca5211357cefcb690f3e53f9da71c
maria tsyrkin <maria.tsyrkin@hotmail.com>
2018-12-12 17:32:21 +0100

    unpublishing of meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -22,11 +22,11 @@ class Meal < ApplicationRecord
   validates :description, presence: true, length: { minimum: 10 }
   validates :category, presence: true, inclusion: { in: ["appetizer", "main course", "dessert", "side", "other"]}
 
-  def self.published
-    self.where(published: true)
+  def self.published(meals)
+    meals.where(published: true)
   end
 
-  def self.unpublished
+  def unpublished
     self.where(published: false)
   end
 end
